92eecab3cfd80dc6cabcd05ffbbb0874b6ea2573
Lucasvianaa-cpu <76756737+Lucasvianaa-cpu@users.noreply.github.com>
2022-11-29 22:00:08 -0300

    deploy 2

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ app.get('/user',(request, response)=>{
     });
 })
 
+app.get('/ip',(request, response)=>{
+    const os = require('os');
+    const networkInfo = os.networkInterfaces();
+    const array = networkInfo.eth0[0].address
+    const ip = JSON.stringify(array);
+    
+    
+    return response.end(`IP DO SERVIDOR QUE ESTA RESPONDENDO: ${ip}`);
+})
+
 app.listen(port, () => {
     console.log('Servidor Iniciado')
 })
